71fecf51cd27d3dc70d21aff34e43c74f0c0edfa
Jakub Chojnacki <kbchojnacki@gmail.com>
2019-11-08 01:47:27 +0100

    update

--- test.js
+++ test.js
@@ -5,27 +5,31 @@ const dir = path.dirname(require.main.filename);
 
 const isWin = process.platform === "win32";
 const isLin = process.platform === "linux";
+const initdb = dir+(isWin?"/pgsql/win/bin/initdb.exe":isLin?"/pgsql/lin/bin/initdb":"/pgsql/osx/bin/initdb");
+const pg_ctl = dir+(isWin?"/pgsql/win/bin/pg_ctl.exe":isLin?"/pgsql/lin/bin/pg_ctl":"/pgsql/osx/bin/pg_ctl");
 
 const sleep = (time) => new Promise((res,rej) => setTimeout(() => res(),time));
 
-console.log(dir)
-const initdb = dir+(isWin?"/pgsql/win/bin/initdb.exe":isLin?"/pgsql/lin/bin/initdb":"/pgsql/osx/bin/initdb");
-const pg_ctl = dir+(isWin?"/pgsql/win/bin/pg_ctl.exe":isLin?"/pgsql/lin/bin/pg_ctl":"/pgsql/osx/bin/pg_ctl");
 
 const rm = (file) => exec('rm -rf ' + dir + '/' + file);
 const clean = async () => { await rm('data');await rm('logfile')};
 const init = () => exec(initdb+' -A  trust -U postgres -D'+dir + '/data -E UTF-8');
-const start = () => exec(pg_ctl+' -D' + dir+ '/data -l logfile start');
-const stop = () => exec(pg_ctl+' -D' + dir + '/data -l logfile stop');
+const pg_start = () => exec(pg_ctl+' -D' + dir+ '/data -l logfile start');
+const pg_stop = () => exec(pg_ctl+' -D' + dir + '/data -l logfile stop');
 
-const test = async () => {
+const start = async () => {
     await clean();
     console.log((await init()).stdout)
-    console.log((await start()).stdout)
-    await sleep(10000);
-    console.log((await stop()).stdout)
+    console.log((await pg_start()).stdout)
+}
+
+const stop = async () => {
+    console.log((await pg_stop()).stdout)
     await clean();
 }
 
-test();
+module.exports = {
+    start:start,
+    stop:stop
+}
 
